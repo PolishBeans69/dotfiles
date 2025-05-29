f5d7febaf1c2315649c9e3c6055a04f37034c58d
PolishBeans69 <romanovgoldov@gmail.com>
2025-05-29 10:28:21 +0200

    Thu May 29 10:28:21 AM CEST 2025

---
+++ Scripts/egzamin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+(cd /home/SSRA/akaszuba/folder-folder/Klasa3-folder/INFORMATYKA/selenium/ ; kitty --class skibidi sh -c "./skrypt.py;exec sh")
---
+++ Scripts/plan.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+(cd /home/SSRA/akaszuba/folder-folder/Klasa3-folder/INFORMATYKA/planlekcji/ ; ./plan.py)
